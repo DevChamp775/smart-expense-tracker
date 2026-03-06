8cd3561bfb0b0d411bd791826e21391aae941787
Devesh Roy <devesh9430695319@gmail.com>
2026-03-06 14:46:53 +0530

    fix render port

--- server.js
+++ server.js
@@ -167,7 +167,8 @@ res.json(rows);
 
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-console.log("Server running on http://localhost:3000");
+app.listen(PORT,()=>{
+console.log("Server running on port " + PORT);
 });
